545787c4e8322651a61035df273b7c1c64fd46b2
shramko82 <shramko82@gmail.com>
2014-12-10 23:14:50 +0200

    UTF-8
    Changed format of file 'tolstoy.txt'

--- main/DequeClient.java
+++ main/DequeClient.java
@@ -49,23 +49,6 @@ public class DequeClient {
 					}
 				}
 
-				/*
-				 * boolean isEmptyWord = true; StringBuilder str = new
-				 * StringBuilder(); char ch; for (int i = 0; i < line.length();
-				 * i++) { ch = line.charAt(i); if (Character.isLetter(ch)) {
-				 * str.append(ch); isEmptyWord = false; } if
-				 * ((!Character.isLetter(ch)) || (i == line.length() - 1)) { if
-				 * (!isEmptyWord) { //System.out.println(str); str = new
-				 * StringBuilder(); isEmptyWord = true; n++; Random rand = new
-				 * Random(); int intRand; if (n < 100) { deque.pushLast(str);
-				 * added++; }
-				 * 
-				 * else { intRand = rand.nextInt(4); if (intRand == 0) {
-				 * deque.pushLast(str); added++; } else if (intRand == 1) {
-				 * deque.pushFirst(str); added++; } else if (intRand == 2) {
-				 * deque.popFirst(); taken++; } else if (intRand == 3) {
-				 * deque.popFirst(); taken++; } } } } }
-				 */
 			}
 			Date d2 = new Date();
 			System.out.println("Total words " + n);
@@ -85,7 +68,7 @@ public class DequeClient {
 		try {
 			File file = new File("1\\tolstoy.txt");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					new FileInputStream(file), "Windows-1251"));
+					new FileInputStream(file), "UTF-8"));
 			processFile(reader);
 
 			reader.close();
--- main/StackClient.java
+++ main/StackClient.java
@@ -41,46 +41,7 @@ public class StackClient {
 							taken++;
 						}
 					}
-
 				}
-				/*
-				boolean isEmptyWord = true;
-				StringBuilder str = new StringBuilder();
-				char ch;
-				for (int i = 0; i < line.length(); i++) {
-					ch = line.charAt(i);
-					if (Character.isLetter(ch)) {
-						str.append(ch);
-						isEmptyWord = false;
-					}
-					if ((!Character.isLetter(ch)) || (i == line.length() - 1)) {
-						if (!isEmptyWord) {
-							// System.out.println(str);
-							str = new StringBuilder();
-							isEmptyWord = true;
-							n++;
-							Random rand = new Random();
-							int intRand;
-							if (n < 100) {
-								stack.push(str);
-								added++;
-							}
-
-							else {
-								intRand = rand.nextInt(2);
-								if (intRand == 1) {
-									stack.push(str);
-									added++;
-								}
-
-								else {
-									stack.pop();
-									taken++;
-								}
-							}
-						}
-					}
-				}*/
 			}
 			Date d2 = new Date();
 			System.out.println("Total words " + n);
@@ -100,7 +61,7 @@ public class StackClient {
 		try {
 			File file = new File("1\\tolstoy.txt");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					new FileInputStream(file), "Windows-1251"));
+					new FileInputStream(file), "UTF-8"));
 			processFile(reader);
 
 			reader.close();
